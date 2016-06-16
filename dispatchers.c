ef0582a5e6b6f0082461f56c4e426cdad317e3ce
Jack Romo <sharrackor@gmail.com>
2016-06-16 15:09:28 +0100

    Add dispatchers.h file

---
+++ dispatchers.c
@@ -0,0 +1,46 @@
+/*
+ * File: dispatchers.c
+ *
+ *  in CNoodle.
+ *
+ * Author: Jack Romo <sharrackor@gmail.com>
+ */
+
+#include "cnoodle.h"
+
+
+void cmd_alter_entity(t_game_data *data, struct alter_entity_command cmd) {
+    //
+}
+
+void cmd_add_entity(t_game_data *data, struct add_entity_command cmd) {
+    //
+}
+
+void cmd_rem_entity(t_game_data *data, struct rem_entity_command cmd) {
+    //
+}
+
+void cmd_alter_room(t_game_data *data, struct alter_room_command cmd) {
+    //
+}
+
+void cmd_next_room(t_game_data *data, struct next_room_command cmd) {
+    //
+}
+
+void cmd_play_sound(t_game_data *data, struct play_sound_command cmd) {
+    //
+}
+
+void cmd_pause_sound(t_game_data *data, struct pause_sound_command cmd) {
+    //
+}
+
+void cmd_end_sound(t_game_data *data, struct end_sound_command cmd) {
+    //
+}
+
+void cmd_quit(t_game_data *data, struct quit_command cmd) {
+    //
+}
