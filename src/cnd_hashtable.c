c25dfb499815e4e1d085a2836c6800c46bd5a4b3
Jack Romo <sharrackor@gmail.com>
2016-08-15 13:33:54 +0200

    Update docs for hashtable

--- src/cnd_hashtable.c
+++ src/cnd_hashtable.c
@@ -2,6 +2,7 @@
  * File: cnd_hashtable.c
  *
  * Contains all source code for hashtables.
+ * Hashtables are used by t_game_data to store entities, rooms, etc.
  *
  * Author: Jack Romo <sharrackor@gmail.com>
  */
@@ -11,9 +12,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+ * make_hashtable: Create a hashtable.
+ *
+ * num_elems (int): Number of linked lists in hashtable.
+ *
+ * Returns (hashtable): Empty hashtable.
+ */
 hashtable make_hashtable(int num_elems) {
     hashtable table;
-    llist_node** list = (llist_node**) malloc(sizeof(llist_node*)*num_elems);
+    llist_node** list = malloc(sizeof(llist_node*)*num_elems);
     if(list == NULL) {
         perror("Could not allocate list for hashtable.");
         exit(EXIT_FAILURE);
@@ -23,26 +31,41 @@ hashtable make_hashtable(int num_elems) {
     return table;
 }
 
+/*
+ * hash: Private method for hashing an ID.
+ */
 int hash(int id, hashtable table) {
     return id % (table.num_elems);
 }
 
+/*
+ * hashtable_add: Add an element to a hashtable of any type.
+ */
 void hashtable_add(hashtable table, void* elem, elem_type type) {
     llist_node elem_node = make_node(elem, type);
     int entry_pos = hash(get_id(elem, type), table);
     add_node(&table.list[entry_pos], elem_node);
 }
 
+/*
+ * hashtable_get: Get a void* to an element with an ID in a hashtable.
+ */
 void *hashtable_get(hashtable table, int id) {
     int entry_pos = hash(id, table);
     return get_node(table.list[entry_pos], id).elem;
 }
 
+/*
+ * hashtable_del: Delete an element with an ID in a hashtable.
+ */
 void hashtable_del(hashtable table, int id) {
     int entry_pos = hash(id, table);
     del_node(&table.list[entry_pos], id);
 }
 
+/*
+ * hashtable_contains: Return true if contains an ID, false otherwise.
+ */
 bool hashtable_contains(hashtable table, int id) {
     for(int i = 0; i < table.num_elems; i++) {
         if(llist_contains(table.list[i], id))
@@ -51,6 +74,9 @@ bool hashtable_contains(hashtable table, int id) {
     return false;
 }
 
+/*
+ * hashtable_get_ids: Get array of all IDs in hashtable.
+ */
 int *hashtable_get_ids(hashtable table) {
     int **id_arrays = (int **) malloc(sizeof(int*)*table.num_elems);
     if(id_arrays == NULL) exit(EXIT_FAILURE);
@@ -75,6 +101,9 @@ int *hashtable_get_ids(hashtable table) {
     return ids;
 }
 
+/*
+ * hashtable_get_num_entries: Get number of entries in hashtable.
+ */
 int hashtable_get_num_entries(hashtable table) {
     int length = 0;
     for(int i = 0; i < table.num_elems; i++) {
@@ -83,6 +112,9 @@ int hashtable_get_num_entries(hashtable table) {
     return length;
 }
 
+/*
+ * hashtable_free: Free all memory in hashtable.
+ */
 void hashtable_free(hashtable table) {
     for(int i = 0; i < table.num_elems; i++) {
         llist_free(table.list[i]);
--- src/cnd_hashtable.h
+++ src/cnd_hashtable.h
@@ -20,22 +20,16 @@ typedef struct {
     // when multithreading added, put lock for each linked list
 } hashtable;
 
+// All hashtable functions (see cnd_hashtable.c)
+
 hashtable make_hashtable(int num_elems);
-
 int hash(int id, hashtable table);
-
 void hashtable_add(hashtable table, void* elem, elem_type type);
-
 void *hashtable_get(hashtable table, int id);
-
 void hashtable_del(hashtable table, int id);
-
 bool hashtable_contains(hashtable table, int id);
-
 int *hashtable_get_ids(hashtable table);
-
 int hashtable_get_num_entries(hashtable table);
-
 void hashtable_free(hashtable table);
 
 #endif //CND_HASHTABLE_H
